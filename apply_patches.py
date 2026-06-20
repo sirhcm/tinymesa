1875281cae78ca9cc645dd6ea3e7d486b3d49832
Christopher Milan <chrismilan@ucla.edu>
2026-06-19 17:52:18 -0700

    fix

--- apply_patches.py
+++ apply_patches.py
@@ -3,8 +3,8 @@ import sys, os, pathlib, email.parser
 from packaging.version import Version
 from packaging.specifiers import SpecifierSet
 
-assert len(sys.argv) == 2, f"usage: {sys.argv[0]} MESA_VER"
-mesa_ver = Version(sys.argv[1])
+assert len(sys.argv) == 2, f"usage: {sys.argv[0]} MESA_TAG"
+mesa_ver = Version(sys.argv[1].split('-')[1])
 parser = email.parser.HeaderParser()
 
 for p in (pathlib.Path(__file__).parent / 'patches').iterdir():
