88d461de52ac70b43c58de152c798846dfecbd50
Christopher Milan <chrismilan@ucla.edu>
2026-07-02 13:08:34 -0700

    distribute via pypi (#7)

--- apply_patches.py
+++ apply_patches.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
-import sys, os, pathlib, email.parser
+import shutil, sys, subprocess, pathlib, platform, email.parser
 from packaging.version import Version
 from packaging.specifiers import SpecifierSet
 
-assert len(sys.argv) == 2, f"usage: {sys.argv[0]} MESA_TAG"
-mesa_ver = Version(sys.argv[1].split('-')[1])
+assert len(sys.argv) == 3, f"usage: {sys.argv[0]} MESA_DIR MESA_TAG"
+mesa_dir = pathlib.Path(sys.argv[1])
+mesa_ver = Version(sys.argv[2].split('-')[1])
 parser = email.parser.HeaderParser()
+
+def system(cmd): subprocess.run(cmd.split(), check=True, cwd=mesa_dir)
 
 for p in (pathlib.Path(__file__).parent / 'patches').iterdir():
   if mesa_ver in SpecifierSet(parser.parse(p.open()).get('Mesa-Version', '')):
-    os.system(f"patch -p1 --fuzz=3 -i {p}")
+    system(f"patch -p1 --fuzz=3 -i {p}")
+
+if platform.system() == "Darwin": (mesa_dir / "include/xf86drm.h").touch()
+
+for p in [mesa_dir / "src" / s / "meson.build" for s in ["compiler", "compiler/nir", "gallium/auxiliary", "nouveau/compiler", "util", "freedreno/ir3", "freedreno/common"]]:
+  p.write_text("\n".join(filter(lambda l: "gnu_symbol_visibility" not in l, p.read_text().splitlines())))
+
+with (mesa_dir / "src/meson.build").open("a") as f: f.write("subdir('tinymesa')\n")
+
+(mesa_dir / "src/tinymesa").mkdir(exist_ok=True)
+shutil.copy(pathlib.Path(__file__).parent / "meson.build", mesa_dir / "src/tinymesa/meson.build")
---
+++ build_wheel.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import argparse, pathlib, shutil, tempfile
+from distlib.wheel import Wheel
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--tag")
+parser.add_argument("--sha")
+parser.add_argument("mesa_tag")
+
+args, files = parser.parse_known_args()
+if args.tag: assert args.tag.startswith('v') and args.tag[1:].isdigit(), "expected tag of form 'vN'"
+else: assert args.sha, "expected one of --tag or --sha to be set"
+
+version = args.mesa_tag.split('-')[1] + (f".{args.tag[1:]}" if args.tag else f"+{args.sha}")
+files = [pathlib.Path(f) for f in files]
+
+init = "\n".join(["import pathlib", "root = pathlib.Path(__file__).parent"] +
+                 [f"{f.stem} = root / {f.name!r}" for f in files] + ["__all__ = [" + ", ".join([repr(f.stem) for f in files]) + "]"])
+meta = f"""Metadata-Version: 2.1
+Name: tinymesa
+Version: {version}
+Summary: mesa, but tiny
+Requires-Python: >=3.8
+"""
+
+with tempfile.TemporaryDirectory() as td:
+  prefix = pathlib.Path(td)
+  platlib = prefix / "platlib"
+  (platlib / "tinymesa").mkdir(parents=True, exist_ok=True)
+  (platlib / "tinymesa/__init__.py").write_text(init)
+  for f in files: shutil.copy(f, platlib / "tinymesa" / f.name)
+  (platlib / f"tinymesa.dist-info").mkdir()
+  (platlib / f"tinymesa.dist-info/METADATA").write_text(meta)
+
+  wheel = Wheel()
+  pathlib.Path("dist").mkdir(exist_ok=True)
+  wheel.name, wheel.version, wheel.dirname = "tinymesa", version, "dist"
+  outfile = wheel.build({"prefix": str(prefix), "platlib": str(platlib)}, tags={"pyver": ["py3"], "abi": ["none"]})
+  print(f"successfully build {outfile}")
